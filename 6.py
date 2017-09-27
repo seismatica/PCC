d29cf2a69e4b491cee96c279a3555f260ad42144
seismatica <nguyenduykhanh.hcm@gmail.com>
2017-09-28 04:01:51 +0700

    Add conditions for quit, rearrange function locations

--- 6.py
+++ 6.py
@@ -3,27 +3,31 @@ def city_country(city, country):
     print(city + ", " + country)
 
 
+def notify_invalid_input(user_input):
+    """This function notifies user if their input was invalid (" " or "")"""
+    if user_input == " " or user_input == "":
+        print("Invalid input.\n")
+
+
 def city_input():
     """This function asks user to input a city and a country, and display them using city_country function"""
     active = True
     while active:
         city = input("Input your city: ")
         if city == " " or city == "":
-            notify_invalid_input(city)
+            notify_invalid_input(city)  # returns to city input prompt
+        elif city.lower() == "quit":
+            active = False  # exits city input loop
         else:
             while active:
                 country = input("Input your country: ")
                 if country == " " or country == "":
-                    notify_invalid_input(country)
+                    notify_invalid_input(country)  # and returns to country input prompt
+                elif country.lower() == "quit":
+                    active = False  # exits both country and city input loops
                 else:
                     city_country(city, country)
-                    active = False
-
-
-def notify_invalid_input(user_input):
-    """This function notifies user if their input was invalid (" " or "")"""
-    if user_input == " " or user_input == "":
-        print("Invalid input.\n")
+                    active = False  # exits both country and city input loops
 
 
 city_input()
