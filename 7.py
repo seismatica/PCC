e3966a703083cebdfe5a35a7a3d1373368608eb5
seismatica <nguyenduykhanh.hcm@gmail.com>
2017-09-28 05:05:43 +0700

    Build function make_album

--- 7.py
+++ 7.py
@@ -1,1 +1,8 @@
-print("Local edit")
+def make_album(artist, title):
+    album_info = {"name": artist, "title": title}
+    return album_info
+
+
+print(make_album("Seal", "Kiss for a rose"))
+print(make_album("Reba", "Fancy"))
+print(make_album("Dolly", "Jolene"))
