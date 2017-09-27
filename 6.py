8349381231a34875d246c08054217865e7745db7
seismatica <nguyenduykhanh.hcm@gmail.com>
2017-09-28 01:45:29 +0700

    Implement city_country function

--- 6.py
+++ 6.py
@@ -1,5 +1,12 @@
 def city_country(city, country):
+    """This function takes in a city and a country, and outputs them next to each other, separate by a comma"""
     print(city + ", " + country)
 
 
-city_country("Ho Chi Minh City", "Vietnam")
+def user_input():
+    """This function asks user to input a city and a country, and display them using city_country function"""
+    city = input("Input your city: ")
+    country = input("Input your country: ")
+    city_country(city, country)
+
+user_input()
