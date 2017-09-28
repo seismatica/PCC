9d3d607940ca2f279ebcc4df8059f928a6694f3e
seismatica <nguyenduykhanh.hcm@gmail.com>
2017-09-28 17:14:22 +0700

    Build function show_magicians

---
+++ 8.py
@@ -0,0 +1,8 @@
+def show_magicians(magicians):
+    """Receive a list of magician and print their names"""
+    for magician in magicians:
+        print(magician)
+
+
+magicians = ["David Copperfield", "David Blaine", "Criss Angel"]
+show_magicians(magicians)
