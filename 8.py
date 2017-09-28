4a9d6020668745d79d45547788ab10abc81e222e
seismatica <nguyenduykhanh.hcm@gmail.com>
2017-09-28 17:17:39 +0700

    Capitalize magician's name

--- 8.py
+++ 8.py
@@ -1,7 +1,7 @@
 def show_magicians(magicians):
     """Receive a list of magician and print their names"""
     for magician in magicians:
-        print(magician)
+        print(magician.title())
 
 
 magicians = ["David Copperfield", "David Blaine", "Criss Angel"]
