c79e2e1133b7c76c21c991cde48173f8f095adfd
seismatica <nguyenduykhanh.hcm@gmail.com>
2017-10-04 12:35:18 +0700

    Consolidate chapters, and add book data files as submodule

--- 16.py
+++
@@ -1,9 +0,0 @@
-from car_generator import make_car as mc
-
-
-def main():
-    subaru = mc("subaru", "outback", color="blue", tow_package=True)
-    print(subaru)
-
-
-main()
--- 8.py
+++
@@ -1,8 +0,0 @@
-def show_magicians(magicians):
-    """Receive a list of magician and print their names"""
-    for magician in magicians:
-        print(magician.title())
-
-
-magicians = ["David Copperfield", "David Blaine", "Criss Angel"]
-show_magicians(magicians)
